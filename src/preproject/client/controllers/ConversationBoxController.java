460494bd633a618102f522afcd4ad921b518122a
Jp De los Trinos <anona19@protonmail.com>
2021-03-01 12:09:26 +0800

    add: custom favorite button (heart) variables to conversationBox controller

--- src/preproject/client/controllers/ConversationBoxController.java
+++ src/preproject/client/controllers/ConversationBoxController.java
@@ -1,19 +1,22 @@
 package preproject.client.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.shape.Circle;
-import javafx.scene.text.Text;
 
 public class ConversationBoxController {
 
     @FXML
-    private Text groupAlias_label;
+    private Label groupAlias_label;
 
     @FXML
-    private RadioButton favorite_button;
+    private Label unreadMsgs_label;
 
     @FXML
     private Circle group_picture;
 
+    @FXML
+    private Button favorite_button;
 }

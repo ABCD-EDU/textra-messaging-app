968fe6cdb9b856832b7f05bebb21792fc27ef4b9
Jp De los Trinos <anona19@protonmail.com>
2021-03-01 11:55:27 +0800

    add: conversationHeader styling added info and delete button (no functionalities) created controller for conversationHeader.fxml

---
+++ src/preproject/client/controllers/ConversationHeaderController.java
@@ -0,0 +1,24 @@
+package preproject.client.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+
+public class ConversationHeaderController {
+    @FXML
+    private Pane header_pane;
+
+    @FXML
+    private Circle header_picture;
+
+    @FXML
+    private Label header_label;
+
+    @FXML
+    private Button info_button;
+
+    @FXML
+    private Button delete_button;
+}
